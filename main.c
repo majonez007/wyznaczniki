f8b75beb7dacd41e2067a7b0edb3b21a36ca264a
majonez007 <maj0nez@poczta.fm>
2017-11-23 10:17:11 +0100

    main trzecia poprawka

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 
-void wczytaj(double *a1, double *b1, double *c1, double *a2, double *b2, double *c2){ 
+void wczytaj(double *a1, double *b1, double *c1, double *a2, double *b2, double *c2)
+{ 
 
 	puts("Podaj wspołczynnik a1"); scanf("%lf", a1);
     puts("Podaj wspołczynnik b1"); scanf("%lf", b1);
@@ -10,18 +11,33 @@ void wczytaj(double *a1, double *b1, double *c1, double *a2, double *b2, double 
     puts("Podaj wspołczynnik b2"); scanf("%lf", b2);
     puts("Podaj wspołczynnik c2"); scanf("%lf", c2);
 
+{
 
-
-
+    double wyznacznik_glowny(double a1, double a2, double b1, double b2)
+    return a*b2 - a2*b1;
+}    
 
 {
 
+    double wyznacznik_x(double c1, double c2, double b1, double b2)
+    return c1*b2 - c2*b1;
+
+    }
+{    
+    double wyznacznik_y(double a1, double a2, double c1, double c2)
+    return a1*c2 - a2*c1;
+}
+    
 int main(){
     
     
     double a1, b1, c1, a2, b2, c2;
-    
+    double W, Wx, Wy;
     wczytaj (&a1, &b1, &c1, &a2, &b2, &c2);
-
+    W = wyznacznik_glownny(a1, a2, b1, b2);
+    Wx = wyznacznik_x(c1, c2, b1, b2);
+    Wy = wyznacznik_y(a1, a2, c1, c2);
+    x = Wx / W;
+    y = Wy / W;
     return 0;
 }
