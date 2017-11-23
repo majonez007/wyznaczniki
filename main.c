fe6aef8f394e017b0d0f9d0eca31735676410c12
majonez007 <maj0nez@poczta.fm>
2017-11-23 10:00:56 +0100

    main druga poprawka

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
-int main(){
 
+void wczytaj(double *a1, double *b1, double *c1, double *a2, double *b2, double *c2){ 
+
+	puts("Podaj wspołczynnik a1"); scanf("%lf", a1);
+    puts("Podaj wspołczynnik b1"); scanf("%lf", b1);
+    puts("Podaj wspołczynnik c1"); scanf("%lf", c1);
+    puts("Podaj wspołczynnik a2"); scanf("%lf", a2);
+    puts("Podaj wspołczynnik b2"); scanf("%lf", b2);
+    puts("Podaj wspołczynnik c2"); scanf("%lf", c2);
+
+
+
+
+
+{
+
+int main(){
+    
+    
     double a1, b1, c1, a2, b2, c2;
     
-	puts("Podaj wspołczynnik a1"); scanf("%lf", &a1);
-    puts("Podaj wspołczynnik b1"); scanf("%lf", &b1);
-    puts("Podaj wspołczynnik c1"); scanf("%lf", &c1);
-    puts("Podaj wspołczynnik a2"); scanf("%lf", &a2);
-    puts("Podaj wspołczynnik b2"); scanf("%lf", &b2);
-    puts("Podaj wspołczynnik c2"); scanf("%lf", &c2);
+    wczytaj (&a1, &b1, &c1, &a2, &b2, &c2);
 
     return 0;
 }
