36af8c1fa0da9c78ddea2d0d86b8ff2f19dfa270
majonez007 <maj0nez@poczta.fm>
2017-11-23 09:29:30 +0100

    main 2

--- main.c
+++ main.c
@@ -4,12 +4,12 @@ int main(){
 
     double a1, b1, c1, a2, b2, c2;
     
-	puts("Podaj wspołczynnik a1"); scanf("%ld", &a1);
-    puts("Podaj wspołczynnik b1"); scanf("%ld", &b1);
-    puts("Podaj wspołczynnik c1"); scanf("%ld", &c1);
-    puts("Podaj wspołczynnik a2"); scanf("%ld", &a2);
-    puts("Podaj wspołczynnik b2"); scanf("%ld", &b2);
-    puts("Podaj wspołczynnik b3"); scanf("%ld", &b3);
+	puts("Podaj wspołczynnik a1"); scanf("%lf", &a1);
+    puts("Podaj wspołczynnik b1"); scanf("%lf", &b1);
+    puts("Podaj wspołczynnik c1"); scanf("%lf", &c1);
+    puts("Podaj wspołczynnik a2"); scanf("%lf", &a2);
+    puts("Podaj wspołczynnik b2"); scanf("%lf", &b2);
+    puts("Podaj wspołczynnik c2"); scanf("%lf", &c2);
 
     return 0;
 }
