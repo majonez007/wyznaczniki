55ed59a284fd7b48ef4ea7f2ff59bfcde0a82ce7
majonez007 <maj0nez@poczta.fm>
2017-11-23 09:25:18 +0100

    main wyswietlanie

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    double a1, b1, c1, a2, b2, c2;
+    
+	puts("Podaj wspołczynnik a1"); scanf("%ld", &a1);
+    puts("Podaj wspołczynnik b1"); scanf("%ld", &b1);
+    puts("Podaj wspołczynnik c1"); scanf("%ld", &c1);
+    puts("Podaj wspołczynnik a2"); scanf("%ld", &a2);
+    puts("Podaj wspołczynnik b2"); scanf("%ld", &b2);
+    puts("Podaj wspołczynnik b3"); scanf("%ld", &b3);
+
+    return 0;
+}
